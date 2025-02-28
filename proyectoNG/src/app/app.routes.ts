fdadaccaa25608d4bf84a27374e30356b98aae0e
Alejandro1203 <alefg2003@gmail.com>
2025-02-28 14:11:37 +0100

    Ya va la ruta

--- proyectoNG/src/app/app.routes.ts
+++ proyectoNG/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { DepartamentoListComponent } from './features/departamento/components/departamento-list/departamento-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'departamentos', component: DepartamentoListComponent, title: 'Departamentos'}
+];
---
+++ proyectoNG/src/app/features/departamento/components/departamento-list/departamento-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-departamento-list',
+  imports: [CommonModule],
+  templateUrl: './departamento-list.component.html',
+  styleUrl: './departamento-list.component.css'
+})
+export class DepartamentoListComponent {
+
+
+}
