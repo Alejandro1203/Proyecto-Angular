eade9101a09b5bb99063417998b523dd98378fdd
Alejandro1203 <alefg2003@gmail.com>
2025-02-28 17:29:51 +0100

    Another commit

--- proyectoNG/src/app/app.routes.ts
+++ proyectoNG/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { DepartamentoListComponent } from './features/departamento/components/departamento-list/departamento-list.component';
 
 export const routes: Routes = [
+    { path: '', component: HomeComponent, title: 'Home'},
     { path: 'departamentos', component: DepartamentoListComponent, title: 'Departamentos'}
 ];
