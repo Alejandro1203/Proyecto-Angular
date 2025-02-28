7868c4c4d8bcc650bb54c7ee9657d8f1aaf500af
Alejandro1203 <alefg2003@gmail.com>
2025-02-28 22:09:06 +0100

    Lista Empleados por departamento

--- proyectoNG/src/app/features/empleado/components/empleado-list/empleado-list.component.ts
+++ proyectoNG/src/app/features/empleado/components/empleado-list/empleado-list.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Empleado } from '../../../../core/models/empleado.model';
 import { EmpleadoService } from '../../../../core/services/empleado.service';
+import { Departamento } from '../../../../core/models/departamento.model';
+import { DepartamentoService } from '../../../../core/services/departamento.service';
 
 @Component({
   selector: 'app-empleado-list',
@@ -11,8 +13,20 @@ import { EmpleadoService } from '../../../../core/services/empleado.service';
 })
 export class EmpleadoListComponent {
   empleados: Empleado[] = [];
+  departamentos: Departamento[] = [];
 
-  constructor(private empleadoService: EmpleadoService) {
+  constructor(private empleadoService: EmpleadoService, private departamentoService: DepartamentoService) {
     this.empleados = this.empleadoService.getAllEmpleados();
+    this.departamentos = this.departamentoService.getAllDepartamentos();
   }
+
+  getEmpleadosByDepartamento(): Empleado[] {
+    const slc_departamento = document.getElementById('slc_departamento') as HTMLSelectElement;
+
+    this.empleados = slc_departamento.value === '0' ? this.empleadoService.getAllEmpleados() : this.empleadoService.getEmpleadosByDepartamento(Number(slc_departamento.value));
+
+
+    return this.empleados;
+  }
+
 }
