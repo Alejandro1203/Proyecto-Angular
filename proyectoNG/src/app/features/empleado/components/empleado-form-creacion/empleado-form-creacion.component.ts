a2e6f7f0bca6916f9051e7d3c194cfcc8310881b
Alejandro1203 <alefg2003@gmail.com>
2025-03-01 13:53:51 +0100

    Formulario sin función

--- proyectoNG/src/app/features/empleado/components/empleado-form-creacion/empleado-form-creacion.component.ts
+++ proyectoNG/src/app/features/empleado/components/empleado-form-creacion/empleado-form-creacion.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Departamento } from '../../../../core/models/departamento.model';
+import { DepartamentoService } from '../../../../core/services/departamento.service';
+import { FormGroup, ReactiveFormsModule, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-empleado-form-creacion',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './empleado-form-creacion.component.html',
   styleUrl: './empleado-form-creacion.component.css'
 })
 export class EmpleadoFormCreacionComponent {
+  createForm: FormGroup;
+  departamentos: Departamento[] = [];
 
+  constructor(private departamentoService: DepartamentoService, private fb: FormBuilder) {
+    this.departamentos = this.departamentoService.getAllDepartamentos();  
+    
+    this.createForm = this.fb.group({
+      name: ['', [Validators.required, Validators.pattern("^[a-zA-Z0-9]*$")]],
+      jobPosition: ['', Validators.required],
+      idDepartamento: [1 ,Validators.required]
+    });
+  }
 }
